f9b721bc9c50fe324a819b428fe9b0f5824b9cf2
St23154 <e2peyrot@gmail.com>
2023-08-02 20:39:03 +0200

    maintenat j'ai réussi a tout faire marcher

--- fps_este_emilien2.0/Assets/StarterAssets/ThirdPersonController/Scripts/BasicRigidBodyPush.cs
+++ fps_este_emilien2.0/Assets/StarterAssets/ThirdPersonController/Scripts/BasicRigidBodyPush.cs
@@ -1,88 +1,35 @@
 ﻿using UnityEngine;
 
-namespace MyFirstPersonController
+public class BasicRigidBodyPush : MonoBehaviour
 {
-    [RequireComponent(typeof(CharacterController))]
-    public class FirstPersonController : MonoBehaviour
-    {
-        public float MoveSpeed = 5.0f;
-        public float JumpHeight = 1.2f;
-        public float Gravity = -15.0f;
-        public LayerMask GroundLayers;
+	public LayerMask pushLayers;
+	public bool canPush;
+	[Range(0.5f, 5f)] public float strength = 1.1f;
 
-        public bool CanPush = true;
-        public LayerMask PushLayers;
-        [Range(0.5f, 5f)] public float PushStrength = 1.1f;
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if (canPush) PushRigidBodies(hit);
+	}
 
-        private CharacterController _controller;
-        private Vector3 _playerVelocity;
-        private bool _isGrounded;
+	private void PushRigidBodies(ControllerColliderHit hit)
+	{
+		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
 
-        private void Start()
-        {
-            _controller = GetComponent<CharacterController>();
-        }
+		// make sure we hit a non kinematic rigidbody
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body == null || body.isKinematic) return;
 
-        private void Update()
-        {
-            MovePlayer();
-        }
+		// make sure we only push desired layer(s)
+		var bodyLayerMask = 1 << body.gameObject.layer;
+		if ((bodyLayerMask & pushLayers.value) == 0) return;
 
-        private void MovePlayer()
-        {
-            // Check if the player is grounded
-            _isGrounded = Physics.CheckSphere(transform.position, 0.1f, GroundLayers, QueryTriggerInteraction.Ignore);
+		// We dont want to push objects below us
+		if (hit.moveDirection.y < -0.3f) return;
 
-            if (_isGrounded && _playerVelocity.y < 0)
-            {
-                _playerVelocity.y = 0f;
-            }
+		// Calculate push direction from move direction, horizontal motion only
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
 
-            // Get input for movement
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            // Move the player in the desired direction
-            Vector3 moveDirection = transform.forward * moveVertical + transform.right * moveHorizontal;
-            _controller.Move(moveDirection * MoveSpeed * Time.deltaTime);
-
-            // Jump
-            if (_isGrounded && Input.GetButtonDown("Jump"))
-            {
-                _playerVelocity.y += Mathf.Sqrt(JumpHeight * -2f * Gravity);
-            }
-
-            // Apply gravity
-            _playerVelocity.y += Gravity * Time.deltaTime;
-            _controller.Move(_playerVelocity * Time.deltaTime);
-        }
-
-        private void OnControllerColliderHit(ControllerColliderHit hit)
-        {
-            if (CanPush)
-            {
-                PushRigidBodies(hit);
-            }
-        }
-
-        private void PushRigidBodies(ControllerColliderHit hit)
-        {
-            // make sure we hit a non kinematic rigidbody
-            Rigidbody body = hit.collider.attachedRigidbody;
-            if (body == null || body.isKinematic) return;
-
-            // make sure we only push desired layer(s)
-            var bodyLayerMask = 1 << body.gameObject.layer;
-            if ((bodyLayerMask & PushLayers.value) == 0) return;
-
-            // We dont want to push objects below us
-            if (hit.moveDirection.y < -0.3f) return;
-
-            // Calculate push direction from move direction, horizontal motion only
-            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
-
-            // Apply the push and take strength into account
-            body.AddForce(pushDir * PushStrength, ForceMode.Impulse);
-        }
-    }
+		// Apply the push and take strength into account
+		body.AddForce(pushDir * strength, ForceMode.Impulse);
+	}
 }
