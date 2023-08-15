6757c2813b1d0af9ddc4ac1866a0f9f04f3867d1
EmilienB <emiliendollar@gmail.com>
2023-08-15 17:52:53 +0200

    marche

--- fps_este_emilien2.0/Assets/_Scripts/InputManager.cs
+++ fps_este_emilien2.0/Assets/_Scripts/InputManager.cs
@@ -12,15 +12,11 @@ namespace UnityTutorial.Manager
         public Vector2 Move {get; private set;}
         public Vector2 Look {get; private set;}
         public bool Run {get; private set;}
-        public bool Jump {get; private set;}
-        public bool Crouch {get; private set;}
 
         private InputActionMap _currentMap;
         private InputAction _moveAction;
         private InputAction _lookAction;
         private InputAction _runAction;
-        private InputAction _jumpAction;
-        private InputAction _crouchAction;
 
         private void Awake() {
             HideCursor();
@@ -28,20 +24,14 @@ namespace UnityTutorial.Manager
             _moveAction = _currentMap.FindAction("Move");
             _lookAction = _currentMap.FindAction("Look");
             _runAction  = _currentMap.FindAction("Run");
-            _jumpAction = _currentMap.FindAction("Jump");
-            _crouchAction = _currentMap.FindAction("Crouch");
 
             _moveAction.performed += onMove;
             _lookAction.performed += onLook;
             _runAction.performed += onRun;
-            _jumpAction.performed += onJump;
-            _crouchAction.started += onCrouch;
 
             _moveAction.canceled += onMove;
             _lookAction.canceled += onLook;
             _runAction.canceled += onRun;
-            _jumpAction.canceled += onJump;
-            _crouchAction.canceled += onCrouch;
         }
 
         private void HideCursor()
@@ -61,14 +51,6 @@ namespace UnityTutorial.Manager
         private void onRun(InputAction.CallbackContext context)
         {
             Run = context.ReadValueAsButton();
-        }
-        private void onJump(InputAction.CallbackContext context)
-        {
-            Jump = context.ReadValueAsButton();
-        }
-        private void onCrouch(InputAction.CallbackContext context)
-        {
-            Crouch = context.ReadValueAsButton();
         }
 
         private void OnEnable() {
