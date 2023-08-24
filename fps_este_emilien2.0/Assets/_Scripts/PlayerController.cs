c8145e1ed089841ff0ed718b901a3f4203daf294
St23154 <e2peyrot@gmail.com>
2023-08-24 09:27:21 +0200

    saut qui marche plus

--- fps_este_emilien2.0/Assets/_Scripts/PlayerController.cs
+++ fps_este_emilien2.0/Assets/_Scripts/PlayerController.cs
@@ -16,10 +16,15 @@ namespace UnityTutorial.PlayerControl
         [SerializeField] private float BottomLimit = 70f;
 
         [SerializeField] private float MouseSensitivity = 21.9f;
-
+        [SerializeField, Range(10, 500)] private float JumpFactor = 260f;
+        [SerializeField] private float Dis2Ground = 0.8f;
+        [SerializeField] private LayerMask GroundCheck;
+        
+        
         private Rigidbody _playerRigidbody;
         private InputManager _inputManager;
         private Animator _animator;
+        private bool _grounded;
         private bool _hasAnimator;
         private int _xVelHash;
         private int _yVelHash;
@@ -105,6 +110,37 @@ namespace UnityTutorial.PlayerControl
             _animator.SetTrigger(_jumpHash);
         }
 
+        public void JumpAddForce()
+        {
+            _playerRigidbody.AddForce(-_playerRigidbody.velocity.y * Vector3.up, ForceMode.VelocityChange);
+            _playerRigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
+            _animator.ResetTrigger(_jumpHash);
+        }
+
+        private void SampleGround()
+        {
+            if(!_hasAnimator) return;
+
+            RaycastHit hitInfo;
+            if(Physics.Raycast(_playerRigidbody.worldCenterOfMass, Vector3.down, out hitInfo, Dis2Ground + 0.1f, GroundCheck))
+            {
+                //Grounded
+                _grounded = true;
+                SetAnimationGrounding();
+                return;
+            }
+            //Falling
+            Debug.Log(_grounded);
+            _grounded = false;
+            SetAnimationGrounding();
+            return;
+        }
+        
+        private void SetAnimationGrounding()
+        {
+            _animator.SetBool(_fallingHash, !_grounded);
+            _animator.SetBool(_groundHash, _grounded);
+        }
 
     }
 }
